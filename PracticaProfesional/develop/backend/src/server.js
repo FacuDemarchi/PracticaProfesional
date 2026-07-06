be43db4c2260f74d78354b59dd7602eae2faf63d
facu <f.dem1997gmail.com>
2026-07-06 16:50:25 -0300

    deleting duplicate folder because github common issue

--- PracticaProfesional/develop/backend/src/server.js
+++
@@ -1,6 +0,0 @@
-const { app } = require("./app");
-const { env } = require("./config/env");
-
-app.listen(env.port, () => {
-  console.log(`Backend escuchando en http://localhost:${env.port}`);
-});
